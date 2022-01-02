5cb96492af5f8e2f1eba64b2eb8de6f1e6e9666a
Shehab <shehab.abdullah12@gmail.com>
2022-01-02 20:28:40 +0200

    main web page

---
+++ script.js
@@ -0,0 +1,30 @@
+let tog=document.getElementById("tog");
+let item=document.getElementsByClassName("item");
+
+tog.addEventListener("click",function(){
+    for(let i=0;i<item.length;i++){
+
+        item[i].classList.toggle("active");
+    }
+})
+
+// login
+function signup()
+{
+    document.querySelector(".login-form-container").style.cssText = "display: none;";
+    document.querySelector(".signup-form-container").style.cssText = "display: block;";
+    document.querySelector(".container").style.cssText = "background: linear-gradient(to bottom, rgb(6, 108, 224),  rgb(14, 48, 122));";
+    document.querySelector(".button-1").style.cssText = "display: none";
+    document.querySelector(".button-2").style.cssText = "display: block";
+
+};
+
+function login()
+{
+    document.querySelector(".signup-form-container").style.cssText = "display: none;";
+    document.querySelector(".login-form-container").style.cssText = "display: block;";
+    document.querySelector(".container").style.cssText = "background: linear-gradient(to bottom, rgb(6, 108, 224),  rgb(14, 48, 122));";
+    document.querySelector(".button-2").style.cssText = "display: none";
+    document.querySelector(".button-1").style.cssText = "display: block";
+
+}
